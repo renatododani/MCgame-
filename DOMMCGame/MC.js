96b83f446b952f4e8dcd5691f3fddd364505c638
a-brown77 <a.brown7795@gmail.com>
2021-11-25 22:31:23 -0500

    added comments throughout added reset button and changed function slowed card removal

--- DOMMCGame/MC.js
+++ DOMMCGame/MC.js
@@ -1,8 +1,13 @@
 (function () {
   //find game board
   const board = document.querySelector("#gameboard");
+  // declare lastClicked as a variable
   let lastClicked;
+  // initialize "cards" array for function to push to
   let cards = [];
+  // find reset button
+  const resetButton = document.querySelector("#reset");
+
   // function to make cards
   function makeCard(img, cardType) {
     cards.push({
@@ -12,9 +17,13 @@
       type: cardType,
     });
   }
-  //array of card objects
+
   function resetBoard() {
-    //make me less redundant
+    //clear array
+    cards = [];
+    // clear board in DOM
+    board.innerHTML = "";
+    //array of card objects being pushed
     makeCard("MCImgs/AileenWuornos.jpg", "wuornos");
     makeCard("MCImgs/AileenWuornos.jpg", "wuornos");
     makeCard("MCImgs/EdGein.jpg", "gein");
@@ -32,36 +41,53 @@
     makeCard("MCImgs/DennisRader.jpg", "rader");
     makeCard("MCImgs/DennisRader.jpg", "rader");
     //create an HTML card for every card in the array
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < cards.length; i++) {
       board.innerHTML += `
         <div class="card ${cards[i].type}" data-card="${cards[i].type}">
           <img class="front" src="MCImgs/Oak-Leaf-Back.jpg" alt="back of card" />
           <img class="back" src="${cards[i].imageLink}" alt="serialkiller" />
-        </div>
+        </div>   
     `;
     }
   }
   resetBoard();
+
+  // calls reset board function when reset button is clicked
+  resetButton.addEventListener("click", (event) => {
+    resetBoard();
+  });
+
   board.addEventListener("click", (event) => {
     //check if a card is clicked
     if (event.target.className === "front") {
-      //set clicked card
+      //store data-card value of clicked card in a variable
       let newClickedCard = event.target.parentNode.dataset.card;
+
+      // add class "flipped" to div that holds clicked img
       event.target.parentNode.classList.add("flipped");
+
+      // if lastClicked is not undefined and the data-card value of newClicked and lastClicked are the same
       if (lastClicked !== undefined && newClickedCard === lastClicked) {
-        //remove cards from array
-        cards = cards.filter((card) => card.type !== lastClicked);
-        //remove cards from UI
-        let cardsToRemove = document.querySelectorAll(`.${lastClicked}`);
-        for (card of cardsToRemove) {
-          card.remove();
-        }
+        // call set timeout, which delays the running the card removal by 2 seconds
+        setTimeout(function () {
+          //remove cards from array
+          cards = cards.filter((card) => card.type !== lastClicked);
+          //remove cards from UI
+          // querySelectorAll creates an array of cards that will be removed from the array
+          let cardsToRemove = document.querySelectorAll(`.${lastClicked}`);
+          // remove each card from cardsToRemove array
+          for (card of cardsToRemove) {
+            card.remove();
+          }
+        }, 2000);
+      } else {
+        // if card type values do not match, remove class flipped from cards after 3 sec
+        setTimeout(function () {
+          event.target.parentNode.classList.remove("flipped");
+        }, 3000);
       }
-      //rework me to run correctly, make me not run on match
-      setTimeout(function () {
-        event.target.parentNode.classList.remove("flipped");
-      }, 5000);
       //make me undefined or newClicked, depending on match or not
+
       lastClicked = newClickedCard;
     }
   });
